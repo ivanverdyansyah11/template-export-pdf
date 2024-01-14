4db70990d6ed49d1612bb4244f51de66590ab58d
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-01-14 16:51:13 +0800

    Progress: Finish Export PDF Excuse

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,10 @@ class HomeController extends Controller
     public function index() {
         return view('index');
     }
+
+    public function export() {
+
+        $pdf = PDF::loadView('export')->setPaper('a4', 'portrait');
+        return $pdf->stream();
+    }
 }
