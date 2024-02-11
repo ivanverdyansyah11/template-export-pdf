76c55f830e15133a2a37005bdb9dc25d91418d10
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-11 13:35:42 +0800

    Progress: Update Link Redirect Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,50 @@
     <title>Document</title>
 </head>
 <body>
-    
-    <a href="{{ route('export') }}" target="_blank">Print File</a>
+    <a href="{{ route('exportSkLahir') }}" target="_blank" style="margin: 0 24px;">Print Lahir</a>
+    <a href="{{ route('exportSkKawin') }}" target="_blank" style="margin: 0 24px;">Print Kawin</a>
+    <a href="{{ route('exportSkBelumKawin') }}" target="_blank" style="margin: 0 24px;">Print Belum Kawin</a>
+    <a href="{{ route('exportSkDuda') }}" target="_blank" style="margin: 0 24px;">Print Duda</a>
+    <a href="{{ route('exportSkJanda') }}" target="_blank" style="margin: 0 24px;">Print Janda</a>
+    <a href="{{ route('exportSkCerai') }}" target="_blank" style="margin: 0 24px;">Print Cerai</a>
+    <a href="{{ route('exportSKTU') }}" target="_blank" style="margin: 0 24px;">Print SKTU</a>
 
+    <br>
+    <br>
+
+    <a href="{{ route('exportSKTMBayarCerai') }}" target="_blank" style="margin: 0 24px;">Print SKTM Bayar Cerai</a>
+    <a href="{{ route('exportSKTMBedahRumah') }}" target="_blank" style="margin: 0 24px;">Print SKTM Bedah Rumah</a>
+    <a href="{{ route('exportSKTMDisabilitas') }}" target="_blank" style="margin: 0 24px;">Print SKTM Disabilitas</a>
+    <a href="{{ route('exportSKTMJKNKIS') }}" target="_blank" style="margin: 0 24px;">Print SKTM JKN KIS</a>
+    <a href="{{ route('exportSKTMLansia') }}" target="_blank" style="margin: 0 24px;">Print SKTM Lansia</a>
+    <a href="{{ route('exportSKTMSekolah') }}" target="_blank" style="margin: 0 24px;">Print SKTM Sekolah</a>
+
+    <br>
+    <br>
+
+    <a href="{{ route('exportSKBedaNama') }}" target="_blank" style="margin: 0 24px;">Print Beda Nama</a>
+    <a href="{{ route('exportSuratKeterangan') }}" target="_blank" style="margin: 0 24px;">Print Surat Keterangan</a>
+    <a href="{{ route('exportSKPindahLingkungan') }}" target="_blank" style="margin: 0 24px;">Print Pindah Lingkungan</a>
+    <a href="{{ route('exportSKPindahDesa') }}" target="_blank" style="margin: 0 24px;">Print Pindah Desa</a>
+    <a href="{{ route('exportSKPindahKecamatan') }}" target="_blank" style="margin: 0 24px;">Print Pindah Kecamatan</a>
+    <a href="{{ route('exportSKPindahProvinsi') }}" target="_blank" style="margin: 0 24px;">Print Pindah Provinsi</a>
+
+    <br>
+    <br>
+
+    <a href="{{ route('exportSKBepergian') }}" target="_blank" style="margin: 0 24px;">Print Bepergian</a>
+    <a href="{{ route('exportSKTempatTinggal') }}" target="_blank" style="margin: 0 24px;">Print Tempat Tinggal</a>
+    <a href="{{ route('exportSKHargaTanah') }}" target="_blank" style="margin: 0 24px;">Print Harga Tanah</a>
+    <a href="{{ route('exportSKPenghasilanOrtu') }}" target="_blank" style="margin: 0 24px;">Print Penghasilan Ortu</a>
+    <a href="{{ route('exportSilsilah') }}" target="_blank" style="margin: 0 24px;">Print Silsilah</a>
+    <a href="{{ route('exportSKMeninggal') }}" target="_blank" style="margin: 0 24px;">Print Meninggal</a>
+
+    <br>
+    <br>
+
+    <a href="{{ route('exportSuratIzinOrangTua') }}" target="_blank" style="margin: 0 24px;">Print Surat Izin Orang Tua</a>
+    <a href="{{ route('exportHibahSamsat') }}" target="_blank" style="margin: 0 24px;">Print Hibah Samsat</a>
+    <a href="{{ route('exportSuratPembelianSolar') }}" target="_blank" style="margin: 0 24px;">Print Surat Pembelian Solar</a>
+    <a href="{{ route('exportSuratKeteranganDomisili') }}" target="_blank" style="margin: 0 24px;">Print Surat Keterangan Domisili</a>
 </body>
 </html>
