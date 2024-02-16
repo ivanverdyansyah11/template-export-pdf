ae9632f9961ccdf76aa3ec066bc042cbb22604a9
widya Yasa <widyarusmananda15@gmail.com>
2024-02-16 12:27:29 +0800

    make view surat pembagian warisan

---
+++ resources/views/export-surat-pembagian-waris/export.blade.php
@@ -0,0 +1,270 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .wrapper{
+            /*margin: 2cm;*/
+        }
+        .title-surat{
+            font-size: 18px;
+            text-decoration: underline;
+        }
+        .title-surat-wrapper{
+            width: 100%;
+            text-align: center;
+            font-weight: bold;
+            margin-top: 30px;
+            padding-bottom: 3px;
+        }
+        .content-surat{
+            margin-top: 15px;
+            font-size: 14px;
+        }
+        table{
+            font-size: 14px;
+        }
+        .paragraph{
+            text-indent: 20px;
+        }
+        .table-mt{
+            margin-top: 20px;
+        }
+        .last-paragraph{
+            margin-top: 0;
+        }
+        .ttd-text{
+            width: 100%;
+            text-align: center;
+        }
+        .w-full{
+            width: 100%;
+        }
+        .titik-dua{
+            width: 40px;
+            text-align: center;
+        }
+        .title-list{
+            width: 50px;
+        }
+        .table-wrapper .table-mt:first-child{
+            margin-top: 0;
+        }
+        .paragraph-content-wrapper .paragraph:first-child{
+            margin-top: 20px;
+        }
+        .paragraph-content-wrapper .paragraph{
+            margin-top: -13px;
+        }
+        .list-warisan{
+            list-style-type: none;
+        }
+        .list-warisan li{
+            margin-top: 6px;
+        }
+        .list-warisan li:first-child{
+            margin-top: 0;
+        }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+
+    <div class="title-surat-wrapper">
+        <h1 class="title-surat">SURAT PERNYATAAN PEMBAGIAN WARIS</h1>
+    </div>
+    <div class="content-surat">
+        <p   class="paragraph">
+            Yang bertanda tangan dan / atau cap jempol di bawah:
+        </p>
+        <div class="table-wrapper">
+            <div class="table-1 table-mt">
+                <table>
+                    <tr class="list-data">
+                        <td class="title-list">Nama</td>
+                        <td class="titik-dua">:</td>
+                        <td><strong>Putu Yasa</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="title-list">Umur</td>
+                        <td class="titik-dua">:</td>
+                        <td>20 Tahun</td>
+                    </tr>
+                    <tr>
+                        <td class="title-list">No KTP</td>
+                        <td class="titik-dua">:</td>
+                        <td>5107047112600242</td>
+                    </tr>
+                    <tr>
+                        <td class="title-list">Alamat</td>
+                        <td class="titik-dua">:</td>
+                        <td>BTN Taman Asri No A9,Lingkungan  Gede,  Kelurahan Subagan, Kecamatan Karangasemi, Kabupaten Karangasem.</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="table-2 table-mt">
+                <table>
+                    <tr class="list-data">
+                        <td class="title-list">Nama</td>
+                        <td class="titik-dua">:</td>
+                        <td><strong>Putu Yasa</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="title-list">Umur</td>
+                        <td class="titik-dua">:</td>
+                        <td>20 Tahun</td>
+                    </tr>
+                    <tr>
+                        <td class="title-list">No KTP</td>
+                        <td class="titik-dua">:</td>
+                        <td>5107047112600242</td>
+                    </tr>
+                    <tr>
+                        <td class="title-list">Alamat</td>
+                        <td class="titik-dua">:</td>
+                        <td>BTN Taman Asri No A9,Lingkungan  Gede,  Kelurahan Subagan, Kecamatan Karangasemi, Kabupaten Karangasem.</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="table-3 table-mt">
+                <table>
+                    <tr class="list-data">
+                        <td class="title-list">Nama</td>
+                        <td class="titik-dua">:</td>
+                        <td><strong>Putu Yasa</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="title-list">Umur</td>
+                        <td class="titik-dua">:</td>
+                        <td>20 Tahun</td>
+                    </tr>
+                    <tr>
+                        <td class="title-list">No KTP</td>
+                        <td class="titik-dua">:</td>
+                        <td>5107047112600242</td>
+                    </tr>
+                    <tr>
+                        <td class="title-list">Alamat</td>
+                        <td class="titik-dua">:</td>
+                        <td>BTN Taman Asri No A9,Lingkungan  Gede,  Kelurahan Subagan, Kecamatan Karangasemi, Kabupaten Karangasem.</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="paragraph-content-wrapper">
+            <p class="paragraph">Dengan ini menerangkan:</p>
+            <p class="paragraph">
+                Bahwa kami  selaku ahli waris dari almarhum AYUB telah sepakat untuk membagi bidang tanah warisan seluas 520 M2 sesuai dengan data luas tanah yang tercantum dalam Sertifikat Hak Milik no 03280  / Kel Subagan dengan pembagian sebagai berikut :
+            </p>
+            <ul class="list-warisan">
+                <li>Untuk atas nama ISKANDAR dengan luas 520 M2</li>
+                <li>Untuk atas nama ISKANDAR dengan luas 520 M2</li>
+            </ul>
+        </div>
+        <p class="paragraph last-paragraph">
+            Demikian  pernyataan pembagian waris kami buat dengan sebenarnya tanpa tekanan atau paksaaan dari siapapun, apabila ternyata dikemudian hari isi pernyataan ini tidak benar, maka kami masing-masing para ahli waris bersedia mempertanggung jawabkan sesuai ketentuan hukum yang berlaku.
+        </p>
+    </div>
+    <table class="w-full">
+        <tr>
+            <td class="w-full" style="text-align: center"></td>
+            <td class="w-full" style="text-align: center">Subagan,  26 Desember 2019</td>
+        </tr>
+    </table>
+    <div class="w-full">
+        <p style="text-align: center; font-size: 14px"><strong>Dibuat oleh kami para ahli waris,</strong></p>
+    </div>
+    <table class="w-full">
+        <tr class="ahli-waris-name-wrapper">
+            <td style="width: 30px">1.</td>
+            <td style="width: 200px">Sapiah</td>
+            <td class="w-full"> <img width="100px" height="auto" src="{{public_path('assets/ttd.png')}}" alt=""></td>
+        </tr>
+        <tr class="ahli-waris-name-wrapper">
+            <td style="width: 30px">10.</td>
+            <td style="width: 200px">Sapiah</td>
+            <td class="w-full"> <img width="100px" height="auto" src="{{public_path('assets/ttd.png')}}" alt=""></td>
+        </tr>
+        <tr class="ahli-waris-name-wrapper">
+            <td style="width: 30px">100.</td>
+            <td style="width: 200px">Sapiah</td>
+            <td class="w-full"> <img width="100px" height="auto" src="{{public_path('assets/ttd.png')}}" alt=""></td>
+        </tr>
+    </table>
+    <div class="" style="margin-top: 30px;">
+        <table class="w-full">
+            <tr>
+                <td class="w-full" style="text-align: center">Saksi - Saksi</td>
+            </tr>
+        </table>
+        <table class="w-full">
+            <tr class="w-full">
+                <td class="ttd-text">Pemberi Kuasa</td>
+                <td class="ttd-text">Penerima Kuasa,</td>
+            </tr>
+            <tr style="text-align: center">
+                <td style="height: 60px;">
+                    <img width="100px" height="auto" src="{{public_path('assets/ttd.png')}}" alt="">
+                </td>
+                <td style="height: 60px;">
+                    <img width="100px" height="auto" src="{{public_path('assets/ttd.png')}}" alt="">
+                </td>
+            </tr>
+            <tr class="w-full">
+                <td class="ttd-text">
+                    <strong style="font-size: 14px">I GEDE SUASTA ARDIKA</strong>
+                </td>
+                <td class="ttd-text">
+                    <strong style="font-size: 14px">
+                        DODDY CAHYO INDRYAWAN SH
+                    </strong>
+                </td>
+            </tr>
+        </table>
+        <table class="w-full table-mt">
+            <tr>
+                <td class="w-full" style="text-align: center">Mengetahui /  Menguatkan </td>
+            </tr>
+        </table>
+        <table class="w-full table-mt">
+            <tr class="w-full">
+                <td class="ttd-text">
+                    Mengetahui <br>
+                    Lurah Subagan
+                </td>
+                <td class="ttd-text">
+                    Mengetahui <br>
+                    Camat Karangasem
+                </td>
+            </tr>
+            <tr style="text-align: center">
+                <td style="height: 60px;">
+                    <img width="100px" height="auto" src="{{public_path('assets/ttd.png')}}" alt="">
+                </td>
+                <td style="height: 60px;">
+                    <img width="100px" height="auto" src="{{public_path('assets/ttd.png')}}" alt="">
+                </td>
+            </tr>
+            <tr class="w-full">
+                <td class="ttd-text">
+                    <strong>
+                        <span style="text-decoration: underline">IDA KETUT PUTRA SP</span> <br>
+                        NIP 19650404 200604 1 011
+                    </strong>
+                </td>
+                <td class="ttd-text" style="font-size: 14px">
+                    <strong>
+                        <span style="text-decoration: underline">IDA KETUT PUTRA SP</span> <br>
+                        NIP 19650404 200604 1 011
+                    </strong>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+</body>
+</html>
