138c76009384f51b77ead5c56db01bf124416440
widya Yasa <widyarusmananda15@gmail.com>
2024-02-16 13:22:38 +0800

    make view for surat keterangan ahli waris 2

---
+++ resources/views/export-keterangan-ahli-waris/export2.blade.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .wrapper{
+            /*margin: 2cm;*/
+        }
+        .title-surat{
+            font-size: 18px;
+            text-decoration: underline;
+        }
+        .subtitle-surat{
+            font-size: 14px;
+            margin-top: 0;
+        }
+        .title-surat-wrapper{
+            width: 100%;
+            text-align: center;
+            font-weight: bold;
+            margin-top: 30px;
+        }
+        .content-surat{
+            margin-top: -14px;
+            font-size: 14px;
+        }
+        table{
+            font-size: 14px;
+        }
+        .paragraph{
+            text-indent: 20px;
+        }
+        .table-mt{
+            margin-top: 30px;
+        }
+        .last-paragraph{
+            margin-top: -10px;
+        }
+        .ttd-text{
+            width: 100%;
+            text-align: center;
+        }
+        .w-full{
+            width: 100%;
+        }
+        .table-1 tr td{
+            height: 28px;
+        }
+        .table-1 tr:first-child{
+        }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <img src="{{public_path('assets/keterangan-ahli-waris/kop.png')}}" alt="">
+    <div class="title-surat-wrapper">
+        <h1 class="title-surat">SURAT KETERANGAN AHLI WARIS</h1>
+        <h5 class="subtitle-surat">Nomor :29/VII/Ket/2023</h5>
+    </div>
+    <div class="content-surat">
+        <p   class="paragraph">
+            Yang bertanda tangan di bawah ini Lurah Subagan, Kecamatan Karangasem, Kabupaten Karangasem, Propinsi Bali, menerangkan dengan sebenarnya bahwa :
+        </p>
+        <div class="table-wrapper">
+            <div>
+                <table class="table-1">
+                    <tr>
+                        <td>1</td>
+                        <td>Nama</td>
+                        <td>:</td>
+                        <td><strong>Putu Yasa</strong></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Tempat /tanggal lahir  </td>
+                        <td>:</td>
+                        <td>Subagan, 31-12-1960</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Jenis Kelamin</td>
+                        <td>:</td>
+                        <td>Perempuan</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Agama</td>
+                        <td>:</td>
+                        <td>Hindu</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Kewarganegaraan</td>
+                        <td>:</td>
+                        <td>Indonesia</td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Pekerjaan</td>
+                        <td>:</td>
+                        <td>Pedagang</td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>No KTP/KK</td>
+                        <td>:</td>
+                        <td>5107047112600242</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>Alamat</td>
+                        <td>:</td>
+                        <td>Lingkungan Desa  Kelurahan Subagan, Kecamatan Karangasem, Kabupaten Karangasem.</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <p class="paragraph">
+            Berdasarkan surat pengantar Kepala Lingkungan Desa,  Nomor :  02 / LD / I / 2023,              Tanggal 02 Januari 2023,  Bahwa memang benar yang bersangkutan adalah Istri sekaligus Ahli Waris dari
+            <strong>I MADE SUTEJA (Alm)</strong> yang meninggal dunia pada Tanggal 15-12-2022.
+        </p>
+        <p class="paragraph last-paragraph">
+            Demikian surat keterangan ini dibuat dengan sebenarnya untuk dapat dipergunakan Sebagaimana Mestinya.
+        </p>
+    </div>
+    <div class="" style="margin-top: 30px;">
+        <table class="w-full">
+            <tr class="w-full">
+                <td class="ttd-text"></td>
+                <td class="ttd-text">Subagan, 06 Juli 2023</td>
+            </tr>
+            <tr class="w-full">
+                <td class="ttd-text"></td>
+                <td class="ttd-text">Lurah Subagan</td>
+            </tr>
+            <tr style="text-align: center">
+                <td></td>
+                <td style="height: 60px;">
+                    <img width="100" height="auto" src="{{public_path('assets/ttd.png')}}" alt="">
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+</body>
+</html>
