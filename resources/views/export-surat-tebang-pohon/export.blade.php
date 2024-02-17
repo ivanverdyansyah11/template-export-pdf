95f4dbeb965268c220f48e56ea501c6e5ddf0372
widya Yasa <widyarusmananda15@gmail.com>
2024-02-17 20:42:42 +0800

    make view for surat tebang pohon

---
+++ resources/views/export-surat-tebang-pohon/export.blade.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .wrapper{
+            /*margin: 2cm;*/
+        }
+        .title-surat{
+            font-size: 18px;
+            text-decoration: underline;
+        }
+        .subtitle-surat{
+            font-size: 14px;
+            margin-top: 0;
+        }
+        .title-surat-wrapper{
+            width: 100%;
+            text-align: center;
+            font-weight: bold;
+            margin-top: 30px;
+        }
+        .content-surat{
+            margin-top: -14px;
+            font-size: 14px;
+        }
+        table{
+            font-size: 14px;
+        }
+        .paragraph{
+            text-indent: 20px;
+        }
+        .table-mt{
+            margin-top: 30px;
+        }
+        .last-paragraph{
+            margin-top: -10px;
+        }
+        .ttd-text{
+            width: 100%;
+            text-align: center;
+        }
+        .w-full{
+            width: 100%;
+        }
+        p{
+            font-size: 14px;
+        }
+        .table-content tr td:first-child{
+            width: 60px;
+        }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <p>Subangan, 8 Agustus 2023</p>
+    <p style="margin-top: -10px;">Kepada,</p>
+    <p style="margin-top: -10px;">Yth. <br>Kepala Dinas Lingkungan Hidup Kabupaten Karangasem di <u>Amlapura</u></p>
+    <table class="table-hal" style="margin-top: -10px; text-align: left">
+        <tr>
+            <td>Hal</td>
+            <td>:</td>
+            <td>
+                Mohon Bantuan Penataan Pohon
+                Perindang
+            </td>
+        </tr>
+    </table>
+    <div class="content-surat" style="margin-top: 4px;">
+        <p   class="">
+            Dengan hormat, <br>
+            Yang bertanda tangan dibawah ini :
+
+        </p>
+        <div class="table-wrapper" style="margin-top: -10px;">
+            <div class="table-1">
+                <table class="table-content">
+                    <tr>
+                        <td>Nama</td>
+                        <td>:</td>
+                        <td>Putu Yasa</td>
+                    </tr>
+                    <tr>
+                        <td>Pekerjaan</td>
+                        <td>:</td>
+                        <td>Guru</td>
+                    </tr>
+                    <tr>
+                        <td>Alamat</td>
+                        <td>:</td>
+                        <td>
+                            Lingkungan Desa, Kelurahan Subagan, Kecamatan Karangasem,
+                            Kabupaten Karangasem, Provinsi Bali.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>No. HP</td>
+                        <td>:</td>
+                        <td>
+                            082146179273
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <p class="paragraph">
+            Dalam rangka mengantisipasi dan meminimalisasi terjadinya bencana dan musibah yang diakibatkan oleh pohon perindang serta pohon lainnya, maka dengan ini kami mohon bantuan Bapak untuk menata pohon perindang yang ada di depan SDN 2 Subagan, Pura Bale Agung, dan Pura Puseh Desa Adat Subagan.
+        </p>
+        <p class="paragraph last-paragraph">
+            Demikian kami sampaikan atas bantuan dan kerjasamanya kami ucapkan banyak terimakasih.
+        </p>
+    </div>
+    <div class="" style="margin-top: 30px;">
+        <table class="w-full">
+            <tr class="w-full">
+                <td class="ttd-text">Mengetahui</td>
+            </tr>
+            <tr class="w-full">
+                <td class="ttd-text">Kepala Lingkungan Desa</td>
+                <td class="ttd-text">Pemohon</td>
+            </tr>
+            <tr style="text-align: center">
+                <td style="height: 60px;">
+                    <img width="100px" height="auto" src="{{public_path('assets/ttd.png')}}" alt="">
+                </td>
+                <td style="height: 60px;">
+                    <img width="100px" height="auto" src="{{public_path('assets/ttd.png')}}" alt="">
+                </td>
+            </tr>
+            <tr class="w-full">
+                <td class="ttd-text">
+                    <strong style="font-size: 14px"><u>I GEDE SUASTA ARDIKA</u></strong>
+                </td>
+                <td class="ttd-text">
+                    <strong style="font-size: 14px"><u>I GEDE SUASTA ARDIKA</u></strong>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+</body>
+</html>
